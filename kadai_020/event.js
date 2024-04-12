df9d14d661ebdb5d808041924d73dc5f792150cd
riNa-max <73985400+riNa-max@users.noreply.github.com>
2024-04-13 08:33:13 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,10 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', () => {
+    const text1 = document.getElementById('text');
+    text1.style.display = 'none';
+
+    const text2 = document.getElementById('text2');
+    text2.style.display = 'block';
+
+});
